b0ebabd78029756cd0101db50989d21876ff39fc
xiang-qz <2924371960@qq.com>
2022-06-19 02:47:56 +0000

    新增

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+class BubbleSort{
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入测试数据个数");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        Random rd = new Random();
+        for (int i = 0; i < n; i++) {
+            a[i] = rd.nextInt(100);
+        }
+        BubbleSort test = new BubbleSort();
+        int count =test.bubbleSort(a);
+        System.out.println("基本操作次数：" + count);
+    }
+    int bubbleSort(int a[]){
+        int count = 0;//记录基本操作次数
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 1; j < a.length - i; j++) {
+                if(a[j] < a[j-1]){
+                    swap(a[j],a[j-1]);
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+    void swap(int n,int m){
+        int temp = n;
+        n = m;
+        m = temp;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # experiment-1
 实验一  分治算法
+##实验预期结果与实际结果
+###冒泡排序
+*请输入测试数据个数
+gitpod /workspace/experiment-1 (main) $ javac BubbleSort.java
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3435m
+gitpod /workspace/experiment-1 (main) $ java BubbleSort
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3435m
+请输入测试数据个数
+100
+基本操作次数：2698
+gitpod /workspace/experiment-1 (main) $ javac BubbleSort.java
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3435m
+gitpod /workspace/experiment-1 (main) $ java BubbleSort
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3435m
+请输入测试数据个数
+100
+基本操作次数：2209*
